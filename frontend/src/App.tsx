9f8e97136e4a64d4e9629bcd546ab46a49e4fffc
riccardoboe <rboe2000@gmail.com>
2023-05-29 20:47:58 -0500

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+   <div>
+    <header>
+      RBguitars
+    </header>
+    <main>
+
+    </main>
+    <footer>
+      footer
+    </footer>
+   </div>
+  )
+}
+
+export default App
