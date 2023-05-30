fd998594c9a50266be5b09600e7ac71a379b7231
riccardoboe <rboe2000@gmail.com>
2023-05-29 21:34:12 -0500

    9:34

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,44 @@
-import { useState } from 'react'
-import './App.css'
 import { sampleProducts } from './data'
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap'
 
 function App() {
   return (
-    <div>
-      <header>RBguitars.</header>
+    <div className="d-flex flex-column vh-100 ">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>RBguitars.</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sing in
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
-        <ul>
-          {sampleProducts.map((product) => (
-            <li key={product.slug}>
-              <img
-                src={product.image}
-                alt={product.name}
-                className="product-image"
-              />
-              <h2>{product.name}</h2>
-              <p>$ {product.price}</p>
-            </li>
-          ))}
-        </ul>
+        <Container className="mt-3">
+          <Row>
+            {sampleProducts.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h2>{product.name}</h2>
+                <p>$ {product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </main>
-      <footer>footer</footer>
+      <footer>
+        <div className="text-center">footer</div>
+      </footer>
     </div>
   )
 }
