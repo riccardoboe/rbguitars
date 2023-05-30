88420c39e1110e952cd8496c0e7dece8f511a90a
riccardoboe <rboe2000@gmail.com>
2023-05-29 21:14:26 -0500

    products display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
   return (
-   <div>
-    <header>
-      RBguitars
-    </header>
-    <main>
-
-    </main>
-    <footer>
-      footer
-    </footer>
-   </div>
+    <div>
+      <header>RBguitars</header>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>$ {product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
+      <footer>footer</footer>
+    </div>
   )
 }
 
