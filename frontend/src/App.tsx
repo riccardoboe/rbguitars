f0278fc111de12b7fac011bc3951995417638255
riccardoboe <rboe2000@gmail.com>
2023-06-01 17:54:47 -0500

    styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,9 @@ import {
   Button,
   Badge,
   NavDropdown,
+  Form,
+  InputGroup,
+  FormControl,
 } from 'react-bootstrap'
 import { Store } from './Store'
 
@@ -44,56 +47,141 @@ function App() {
       <header>
         <Navbar
           expand="lg"
-          className="d-flex justify-content-between align-items-center"
+          bg="dark"
+          variant="dark"
+          className="d-flex flex-column align-items-stretch p-2 pb-0 mb-3"
         >
-          <LinkContainer to="/">
-            <Navbar.Brand>
-              &nbsp;
-              <img src="../images/rbguitarslogo2.png" /> RBGuitars.
-            </Navbar.Brand>
-          </LinkContainer>
-          <Nav>
-            <Button variant={mode} onClick={switchModeHandler}>
-              <i
-                className={
-                  mode === 'light' ? 'fa fa-toggle-on' : 'fa fa-toggle-off'
-                }
-              ></i>
-            </Button>
+          <div className="d-flex justify-content-between align-content-center">
+            {/* LOGO */}
+            <LinkContainer to="/" className="header-link">
+              <Navbar.Brand>
+                &nbsp;
+                <img src="../images/rbguitarslogo2.png" /> RBGuitars.
+              </Navbar.Brand>
+            </LinkContainer>
+            {/* END LOGO */}
 
-            <Link to="/cart" className="nav-link">
-              <i className="fa fa-shopping-cart"></i>{' '}
-              {cart.cartItems.length > 0 && (
-                <Badge pill bg="success">
-                  {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
-                </Badge>
-              )}
-            </Link>
-            {userInfo ? (
-              <NavDropdown
-                title={userInfo.name}
-                id="basic-nav-dropdown"
-                className="dropdown-menu-start"
-              >
-                <LinkContainer to="/orderhistory">
-                  <NavDropdown.Item>
-                    <i className="fa-solid fa-receipt"> Orders</i>
-                  </NavDropdown.Item>
-                </LinkContainer>
-                <Link
-                  className="dropdown-item"
-                  to="#signout"
-                  onClick={signoutHandler}
+            {/* SEARCH BOX */}
+
+            <Form className="flex-grow-1 d-flex me-auto">
+              <InputGroup>
+                <FormControl
+                  type="text"
+                  name="q"
+                  id="q"
+                  placeholder="Search for something..."
+                  aria-label="Search for something..."
+                  aria-describedby="button-search"
+                ></FormControl>
+                <Button
+                  variant="outline-primary"
+                  type="submit"
+                  id="button-search"
                 >
-                  <i className="fa-solid fa-right-to-bracket"> Logout</i>
+                  <i className="fas fa-search"></i>
+                </Button>
+              </InputGroup>
+            </Form>
+
+            {/* END SEARCH BOX */}
+
+            {/* LIGHT DARK MODE */}
+            <Navbar.Collapse>
+              <Nav className="w-100 justify-content-end">
+                <Link
+                  to="#"
+                  className="nav-link header-link"
+                  onClick={switchModeHandler}
+                >
+                  <i
+                    className={
+                      mode === 'light' ? 'fa fa-toggle-off' : 'fa fa-toggle-on'
+                    }
+                  ></i>{' '}
+                  {mode === 'light' ? 'Light' : 'Dark'}
                 </Link>
-              </NavDropdown>
-            ) : (
-              <Link className="nav-link" to="/signin">
-                <i className="fa fa-user"></i>
+                {/* END LIGHT DARK MODE */}
+
+                {/* USER */}
+                {userInfo ? (
+                  <NavDropdown
+                    className="header-link"
+                    title={`Hello, ${userInfo.name}`}
+                  >
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>
+                        <i className="fa-solid fa-user" />
+                        &nbsp; Account
+                      </NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/orderhistory">
+                      <NavDropdown.Item>
+                        <i className="fa-solid fa-receipt" /> &nbsp; Orders
+                      </NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider />
+                    <Link
+                      className="dropdown-item"
+                      to="#signout"
+                      onClick={signoutHandler}
+                    >
+                      <i className="fa-solid fa-user-minus" /> &nbsp; Sign Out
+                    </Link>
+                  </NavDropdown>
+                ) : (
+                  <NavDropdown
+                    className="header-link"
+                    title={`Log In to your account`}
+                  >
+                    <LinkContainer to="/signin">
+                      <NavDropdown.Item>
+                        <i className="fa-solid fa-right-to-bracket" /> &nbsp;
+                        Login
+                      </NavDropdown.Item>
+                    </LinkContainer>
+                  </NavDropdown>
+                )}
+                {/* END USER */}
+
+                {/* CART */}
+                <Link to="/cart" className="nav-link header-link p-0">
+                  {
+                    <span className="cart-badge">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </span>
+                  }
+                  <svg
+                    fill="#ffffff"
+                    viewBox="130 150 200 300"
+                    width="40px"
+                    height="40px"
+                  >
+                    <path d="M 110.164 188.346 C 104.807 188.346 100.437 192.834 100.437 198.337 C 100.437 203.84 104.807 208.328 110.164 208.328 L 131.746 208.328 L 157.28 313.233 C 159.445 322.131 167.197 328.219 176.126 328.219 L 297.409 328.219 C 306.186 328.219 313.633 322.248 315.951 313.545 L 341.181 218.319 L 320.815 218.319 L 297.409 308.237 L 176.126 308.237 L 150.592 203.332 C 148.426 194.434 140.675 188.346 131.746 188.346 L 110.164 188.346 Z M 285.25 328.219 C 269.254 328.219 256.069 341.762 256.069 358.192 C 256.069 374.623 269.254 388.165 285.25 388.165 C 301.247 388.165 314.431 374.623 314.431 358.192 C 314.431 341.762 301.247 328.219 285.25 328.219 Z M 197.707 328.219 C 181.711 328.219 168.526 341.762 168.526 358.192 C 168.526 374.623 181.711 388.165 197.707 388.165 C 213.704 388.165 226.888 374.623 226.888 358.192 C 226.888 341.762 213.704 328.219 197.707 328.219 Z M 197.707 348.201 C 203.179 348.201 207.434 352.572 207.434 358.192 C 207.434 363.812 203.179 368.183 197.707 368.183 C 192.236 368.183 187.98 363.812 187.98 358.192 C 187.98 352.572 192.236 348.201 197.707 348.201 Z M 285.25 348.201 C 290.722 348.201 294.977 352.572 294.977 358.192 C 294.977 363.812 290.722 368.183 285.25 368.183 C 279.779 368.183 275.523 363.812 275.523 358.192 C 275.523 352.572 279.779 348.201 285.25 348.201 Z" />
+                  </svg>
+                  <span>Cart</span>
+                </Link>
+                {/* END CART */}
+              </Nav>
+            </Navbar.Collapse>
+          </div>
+          {/* SUBHEADER */}
+          <div className="sub-header">
+            <div className="d-flex">
+              <Link to="#" className="nav-link header-link p-1">
+                <i className="fas fa-bars"></i> All
               </Link>
-            )}
-          </Nav>
+              {['Todays Deal', 'Gifts', 'On Sale'].map((x) => (
+                <Link
+                  key={x}
+                  className="nav-link header-link p-1 px-3"
+                  to={`/search?tag=${x}`}
+                >
+                  {x}
+                </Link>
+              ))}
+            </div>
+          </div>
+          {/* END SUBHEADER */}
         </Navbar>
       </header>
       <main>
